61bc374fad84edc94bfa2c899938d7fc294e6131
Huang ShengYu <noahhuang@potix.com>
2013-03-07 10:54:08 +0800

    add checking if the root has one child at most to setParent()

--- src/main/java/org/zkoss/addon/SpaceTreeNode.java
+++ src/main/java/org/zkoss/addon/SpaceTreeNode.java
@@ -4,21 +4,34 @@ import java.util.Collection;
 
 import org.zkoss.zul.DefaultTreeNode;
 
+import test.model.UserDataBean;
+
 public class SpaceTreeNode<E> extends DefaultTreeNode<E> {
 
 	private static int genId = 0;
 	private int id;
+
 	public SpaceTreeNode(E data, Collection<SpaceTreeNode<E>> children) {
 		super(data, children);
 		id = genId++;
 	}
+
+	@Override
+	protected void setParent(DefaultTreeNode<E> parent) {
+		// this method is called after the parent add child, so parent.getChildCount() must be equal or grater than 1
+		if (isRoot(parent) && parent.getChildCount() > 1)
+			try {
+				throw new Exception("the root has one child at most");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		else
+			super.setParent(parent);
+	};
 	
-	public boolean isRoot() {
-		return getParent() == null;	
-	}
-		
+	@Override
 	public void add(org.zkoss.zul.TreeNode<E> child) {
-		if(isRoot() && getChildCount() >= 1)
+		if(isRoot(this) && getChildCount() >= 1)
 			try {
 				throw new Exception("the root has one child at most");
 			} catch (Exception e) {
@@ -27,6 +40,11 @@ public class SpaceTreeNode<E> extends DefaultTreeNode<E> {
 		else
 			super.add(child);
 	};
+
+	private static boolean isRoot(DefaultTreeNode node) {
+		return node != null && node.getData() == null
+				&& node.getParent() == null;
+	}
 
 	public int getId() {
 		return id;
